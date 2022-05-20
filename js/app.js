7e3c5b9749c571a9fd9c482947e4cd06be8b82a8
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-05-20 23:54:07 +0600

    design complete

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const defaultQuotes = [
+    'this is simple quote',
+    'this is simple quote second',
+    'this is simple quote third',
+    'this is simple quote forth',
+    'this is simple quote five'
+];
+window.onload = function () {
+    main();
+}
+
+function main() {
+    let newQuoteBtn = document.getElementById('new-quote-btn');
+    let quoteBody = document.getElementById('quote-body');
+}
